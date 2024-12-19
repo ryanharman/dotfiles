7896c0525ae8366382a3136ce72a046aa37b604c
ryanharman <ryan.harman@chatloop.com>
2024-12-19 13:42:37 +0000

    feat: theme and font

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,22 +1,30 @@
 local wezterm = require("wezterm")
 
+local function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Catppuccin Mocha"
+	else
+		return "Catppuccin Latte"
+	end
+end
+
 return {
-  font_size = 16,
-  font = wezterm.font("CommitMono"),
-  scrollback_lines = 10000,
-  enable_tab_bar = true,
-  hide_tab_bar_if_only_one_tab = true,
-  use_fancy_tab_bar = false,
-  audible_bell = "Disabled",
-  line_height = 1.4,
-  adjust_window_size_when_changing_font_size = false,
-  window_decorations = "RESIZE",
-  window_close_confirmation = "NeverPrompt",
-  -- window_background_opacity = 0.85,
-  -- macos_window_background_blur = 20,
-  term = "xterm-256color",
-  color_scheme = "Tokyo Night",
-  max_fps = 160,
-  -- send_composed_key_when_left_alt_is_pressed = true,
-  -- send_composed_key_when_right_alt_is_pressed = false,
+	font_size = 16,
+	line_height = 1.4,
+	-- font = wezterm.font("CommitMono"),
+	scrollback_lines = 10000,
+	enable_tab_bar = true,
+	hide_tab_bar_if_only_one_tab = true,
+	use_fancy_tab_bar = false,
+	audible_bell = "Disabled",
+	adjust_window_size_when_changing_font_size = false,
+	window_decorations = "RESIZE",
+	window_close_confirmation = "NeverPrompt",
+	-- window_background_opacity = 0.85,
+	-- macos_window_background_blur = 20,
+	term = "xterm-256color",
+	color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
+	max_fps = 160,
+	-- send_composed_key_when_left_alt_is_pressed = true,
+	-- send_composed_key_when_right_alt_is_pressed = false,
 }
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
-return { 
-  {
-    'folke/tokyonight.nvim',
-    priority = 1000, 
-    init = function()
-      vim.cmd.colorscheme 'tokyonight-night'
-    end,
-    }
+return {
+	{
+		"catppuccin/nvim",
+		priority = 1000,
+		init = function()
+			vim.cmd.colorscheme("catppuccin-mocha")
+		end,
+	},
 }
